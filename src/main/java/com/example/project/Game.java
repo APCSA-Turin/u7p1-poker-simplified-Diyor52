f91676bf728993bf983c59899dee9d86673b313a
Diyor52 <diyora2@nycstudents.net>
2025-03-03 16:59:03 +0000

    Diyor push

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -13,7 +13,19 @@ public class Game {
         } else if (p2Rank > p1Rank) {
             return "Player 2 wins!";
         } else {
-           
+            ArrayList<Card> p1Cards = new ArrayList<>(p1.getHand());
+            p1Cards.addAll(communityCards);
+            ArrayList<Card> p2Cards = new ArrayList<>(p2.getHand());
+            p2Cards.addAll(communityCards);
+            for (int i = 0; i < p1Cards.size(); i++) {
+                int p1CardRank = Utility.getRankValue(p1Cards.get(i).getRank());
+                int p2CardRank = Utility.getRankValue(p2Cards.get(i).getRank());
+                if (p1CardRank > p2CardRank) {
+                    return "Player 1 wins!";
+                } else if (p2CardRank > p1CardRank) {
+                    return "Player 2 wins!";
+                }
+            }
 
             return "Tie!";
         }
